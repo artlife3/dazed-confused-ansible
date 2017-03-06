494f048a9096141906c7967c3eb401c5050fd678
Atsushi Iwasaki <a_iwasaki@PANU-MBP.local>
2017-03-06 20:40:01 +0900

    mysql.yml

--- install.sh
+++ install.sh
@@ -17,3 +17,13 @@ else
     ansible --version
 fi
 
+# PING TEST
+echo "---------- Ansible test ping ----------"
+ansible dazed-vagrant -m ping
+
+# ECHO TEST
+echo "---------- Ansible test echo ----------"
+ansible dazed-vagrant -m shell -a 'echo "test"'
+
+
+
